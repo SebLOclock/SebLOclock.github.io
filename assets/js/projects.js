4ae8af107f85a9ae047a737d68d858164d79b61d
Sèb <lemoine.sebastien.cdf@gmail.com>
2024-11-05 16:17:40 +0100

    desktop done

---
+++ assets/js/projects.js
@@ -0,0 +1,17 @@
+const projects = [
+    {
+        id: 1,
+        title: 'Margin Padding',
+        description: 'Une petite interface permettant de visualiser les effets de marges et paddings d\'un élément HTML.',
+        image: 'public/images/demo-m-p.png',
+        url: 'https://sebloclock.github.io/demo-margin-padding/',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    }, {
+        id: 2,
+        title: 'OneCommandWPContainer',
+        description: 'Un outil de script permettant l\'installation et la configuration de WordPress dans un conteneur Docker.',
+        image: 'public/images/docker-wp.png',
+        url: 'https://github.com/Sebloclock/OneCommandWPContainer',
+        technologies: ['Shell', 'linux', 'wordpress', 'docker']
+    }
+];

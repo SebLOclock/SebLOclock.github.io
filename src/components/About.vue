4b36d2a9fe404318ae53d07f91d70182b6d5b944
SebLOclock <sebastien.lemoine@oclock.io>
2025-03-31 17:06:37 +0200

    edit gitignore

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,9 +3,9 @@
     <section id="about" class="section">
       <h2 class="section__title">À propos</h2>
       <div class="about-content">
-        <p>Leader passionné par l'innovation éducative, j'accompagne les talents vers l'excellence. Mon expérience en gestion de projets, pédagogie et technologies me permet d'inspirer et structurer des formations impactantes.</p>
-        <p>Ancien développeur et consultant SI, j'ai une compréhension approfondie des enjeux techniques et managériaux qui me permet d'apporter une valeur ajoutée significative dans la conception de parcours de formation et la gestion d'équipes.</p>
-        <p class="quote">Mon credo : <em>"Former aujourd'hui les leaders de demain en conjuguant innovation technique et excellence pédagogique."</em></p>
+        <p>Manager passionné par l'innovation éducative, j'accompagne les talents vers l'excellence. Mon expérience en gestion de projets, pédagogie et dans le numérique me permettent d'inspirer et structurer des formations adaptées aux besoins des entreprises et des apprenants.</p>
+        <p>Ancien développeur et consultant SI, j'ai une compréhension approfondie des enjeux techniques et managériaux qui me permettent d'apporter une valeur ajoutée significative dans la conception de parcours de formation et la gestion d'équipes.</p>
+        <p class="quote">Mon credo : <em>"Former aujourd'hui les talents de demain en conjuguant innovation technique et excellence pédagogique."</em></p>
       </div>
     </section>
   </IntersectionObserver>
